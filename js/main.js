5e4af382fb6b85c83b1972580ed58b5054ad28f3
EstrellGuyx <guysr2543@gmail.com>
2021-04-18 00:37:45 +0700

    fix doctor and contact

--- js/main.js
+++ js/main.js
@@ -256,3 +256,16 @@ $("#btn-surgery").click(function() {
     $("#service_doctor").hide();
     $("#surgery_doctor").show();
 });
+
+(function($) {
+    $.fn.responsiveTabs = function() {
+        this.addClass('responsive-tabs'),
+            this.append($('<span class="dropdown-arrow"></span>')),
+
+            this.on("click", "li > a.active, span.dropdown-arrow", function() {
+                this.toggleClass('open');
+            }.bind(this)), this.on("click", "li > a:not(.active)", function() {
+                this.removeClass("open")
+            }.bind(this));
+    }
+})(jQuery);
